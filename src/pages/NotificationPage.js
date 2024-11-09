004c92b419c28622e55002422f047fe6f88ea4af
LeoSanchez22 <lsanchezr29@upao.edu.pe>
2024-11-09 09:54:46 -0500

    NotificationPage commit

---
+++ src/pages/NotificationPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NotificationPageMain from "../components/Main/NotificationPageMain";
+
+const OrdersPage = () => {
+    return (
+        <div>
+            <NotificationPageMain />
+        </div>
+    );
+};
+
+export default OrdersPage;
