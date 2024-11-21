f7a60b675039bfb22b43128f072931eb07c8fa2b
alexandercordovaplasencia <alexandercordovaplasencia@gmail.com>
2024-11-21 17:29:09 -0500

    funcionable

--- src/components/Main/Medicamentos.js
+++ src/components/Main/Medicamentos.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
+import { useEffect } from "react";
 
 const Medicamentos = () => {
     const [medicamentos, setMedicamentos] = useState([]);
