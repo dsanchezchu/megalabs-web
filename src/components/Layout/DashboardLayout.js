69181ab7a7a209e997b7193c582d88a26ff67811
Jeffry RU <jeffry.25.ru@gmail.com>
2024-12-04 03:54:52 -0500

    Arreglo al sidebar v2

--- src/components/Layout/DashboardLayout.js
+++ src/components/Layout/DashboardLayout.js
@@ -1,20 +1,21 @@
-// src/layouts/DashboardLayout.js
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar/DashboardPageSidebar';
 import ChatbotButton from '../ChatbotButton/ChatbotButton';
-import './DashboardStyle.css'
+import './DashboardStyle.css';
 
 const DashboardLayout = () => {
-    return (
-        <div className="flex h-screen overflow-hidden">
-            <Sidebar />
-            <div className="flex-grow p-6 bg-gray-100">
-                <Outlet />
-                <ChatbotButton />
-            </div>
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar />
+      <div className="flex-grow p-6 bg-gray-100 overflow-hidden">
+        <div className="content-area">
+          <Outlet />
         </div>
-    );
+        <ChatbotButton />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
