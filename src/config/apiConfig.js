0a68fdac61e139e820c44b16f6db38eebbd3594a
Jeffry RU <jeffry.25.ru@gmail.com>
2024-12-04 03:56:24 -0500

    commit - arreglar calendario

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -3,6 +3,7 @@
 
 export const API_BASE_URL = 'http://localhost:8080/api/v1';
 
+
 // URL del frontend según el entorno
 export const FRONTEND_URL = process.env.NODE_ENV === 'production' 
   ? 'https://megalabs-web.netlify.app/'  // Reemplaza con tu dominio real en Netlify
